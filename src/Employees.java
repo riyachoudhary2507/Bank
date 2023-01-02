113ea0271b960bd4ff6910fd31589dacff9a6bce
Riya <riya03@nagarro.com>
2023-01-02 22:34:54 +0530

    Second commit employees info added

--- src/Employees.java
+++ src/Employees.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
 public class Employees
 {
 	public static void main(String[] args)
 	{
+		int empid;
+		String empname;
+		Scanner scan = new Scanner(System.in);
 		
+		System.out.print("Enter employee id: ");
+		empid = scan.nextInt();
+		System.out.print("Enter employee name: ");
+		empname = scan.nextLine();
+		System.out.println("Employee id: "+empid +"Employee name: "+empname);
 	}
 
 }
