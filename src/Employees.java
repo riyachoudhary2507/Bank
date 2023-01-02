5c118eaee04510fb30b6cd01785a8988f3f43db0
Riya <riya03@nagarro.com>
2023-01-02 23:27:50 +0530

    Final commit

--- src/Employees.java
+++ src/Employees.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Employees
 {
-	static int empid;
+	static int empid, empsalary;
 	static String empname;
 	
 	public static void displayEmployeeInfo()
@@ -12,24 +12,16 @@ public class Employees
 	
 	public static void main(String[] args)
 	{
-<<<<<<< HEAD
-=======
-		int empid, empsalary;
-		String empname;
->>>>>>> user2
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter employee id: ");
 		empid = scan.nextInt();
 		System.out.print("Enter employee name: ");
 		empname = scan.nextLine();
-<<<<<<< HEAD
-		displayEmployeeInfo();
-=======
 		System.out.println("Employee id: "+empid +"Employee name: "+empname);
 		System.out.print("Enter employee salary: ");
 		empsalary = scan.nextInt();
-		
->>>>>>> user2
+		displayEmployeeInfo();
+
 		
 	}
 
