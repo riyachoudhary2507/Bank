0b74075fc4b3e9fbea2c09452521abfa6b3cb164
Riya <riya03@nagarro.com>
2023-01-02 23:11:18 +0530

    Employee info feature added

--- src/Employees.java
+++ src/Employees.java
@@ -2,17 +2,22 @@ import java.util.Scanner;
 
 public class Employees
 {
+	static int empid;
+	static String empname;
+	
+	public static void displayEmployeeInfo()
+	{
+		System.out.println("Employee id: "+empid +"Employee name: "+empname);
+	}
+	
 	public static void main(String[] args)
 	{
-		int empid;
-		String empname;
 		Scanner scan = new Scanner(System.in);
-		
 		System.out.print("Enter employee id: ");
 		empid = scan.nextInt();
 		System.out.print("Enter employee name: ");
 		empname = scan.nextLine();
-		System.out.println("Employee id: "+empid +"Employee name: "+empname);
+		
 	}
 
 }
